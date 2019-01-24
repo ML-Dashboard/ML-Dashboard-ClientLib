a55315a97e9cde0a89f3f316d781abdfbaa668a7
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-01-23 22:04:41 -0600

    Adding lint

---
+++ package/sample.py
@@ -0,0 +1,13 @@
+"""
+Sample
+"""
+
+
+def add(first, second):
+    """
+    Add two things together
+    :param first:
+    :param second:
+    :return:
+    """
+    return first + second
--- test/test_sample.py
+++ test/test_sample.py
@@ -1,6 +1,6 @@
 import pytest
 
-from sample import add
+from package.sample import add
 
 
 @pytest.mark.parametrize('test_input, expected', [
@@ -8,7 +8,6 @@ from sample import add
     ((-1, 2), 1),
     ((1, -2), -1),
     ((0, 0), 0),
-    ((-10, -10), -20)
-])
+    ((-10, -10), -20)])
 def test_add(test_input, expected):
     assert add(*test_input) == expected
